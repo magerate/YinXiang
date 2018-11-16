7b69dc8d645ad4275d9eeef7ba11d7cc9885c783
magerate <magerate1129@gmail.com>
2018-11-16 18:16:12 +0800

    fsadfsa
    nothingfdsa

--- YinXiang/Controllers/BatchController.cs
+++ YinXiang/Controllers/BatchController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using YinXiang.Models;
 
 namespace YinXiang.Controllers
 {
@@ -12,6 +13,22 @@ namespace YinXiang.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult RequestBatchCode(BatchInfo batchInfo)
+        {
+            return Redirect("index");
+            //return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ManualInput(BatchInfo batchInfo)
+        {
+            return Redirect("index");
+            //return View();
         }
     }
 }
---
+++ YinXiang/Models/BatchInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace YinXiang.Models
+{
+    public class BatchInfo
+    {
+        [Display(Name = "ProductType")]
+        public string ProductType { get; set; }
+
+        [Display(Name = "Source")]
+        public string Source { get; set; }
+
+        [Display(Name = "Date")]
+        public DateTime Date { get; set; }
+
+        [Display(Name = "Quantity")]
+        public int Quantity { get; set; }
+
+        [Display(Name = "Code")]
+        public string Code { get; set; }
+    }
+}
