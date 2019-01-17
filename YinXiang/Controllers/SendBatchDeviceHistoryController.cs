483ba63d253255762d717ce9eed6b7862472c435
lemon_kim <suzy_kim@sina.cn>
2019-01-17 23:09:56 +0800

    Batch and SendBatchDeviceHistory

---
+++ YinXiang/Controllers/SendBatchDeviceHistoryController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using YinXiang.Models;
+using YinXiang.Models.Dtos;
+
+namespace YinXiang.Controllers
+{
+    public class SendBatchDeviceHistoryController : Controller
+    {
+        public ApplicationDbContext ApplicationContext
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().Get<ApplicationDbContext>();
+            }
+
+        }
+        // GET: SendBatchDeviceHistory
+        public ActionResult Index()
+        {
+            ViewBag.CreateTime = DateTime.Now.Date;
+            return View();
+        }
+
+        public ActionResult List(BatchDeviceSearchDto search)
+        {
+            if (search == null)
+            {
+                search = new BatchDeviceSearchDto();
+            }
+            var Model = ApplicationContext.SendBatchDeviceHistories.ToList();
+            if (!search.CreateTime.HasValue)
+            {
+                DateTime startBatchDate = search.CreateTime.Value.Date;
+                DateTime endBatchDate = startBatchDate.AddDays(1).AddSeconds(-1);
+                Model.Where(m => m.CreateTime >= startBatchDate && m.CreateTime <= endBatchDate);
+            }
+            if (!string.IsNullOrEmpty(search.BatchNo))
+            {
+                Model = Model.Where(m => m.BatchNo.Contains(search.BatchNo)).ToList();
+            }
+            if (!string.IsNullOrEmpty(search.DeviceName))
+            {
+                Model = Model.Where(m => m.DeviceName.Contains(search.DeviceName)).ToList();
+            }
+            return View(Model);
+        }
+    }
+}
